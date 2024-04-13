2e6958f74288c5872cb3ae69f0a1ee1723afb27f
jan.brkic <jan.brkic@fer.hr>
2024-04-13 17:45:01 +0200

    better button

--- components/morse/button.c
+++ components/morse/button.c
@@ -16,7 +16,6 @@ static const char *TAG = "BUTTON";
 static int64_t last_debounce_time = 0;
 #define DEBOUNCE_DELAY 500000  // Debounce delay in microseconds
 #define MORSE_QUEUE_SIZE (20U)
-#define BUZZER_GPIO 2 // GPIO connected to the buzzer
 
 
 //---------------------------- INTERRUPT HANDLERS ------------------------------
@@ -83,29 +82,6 @@ esp_err_t _button_init(uint8_t pin)
     return esp_err;
 }
 
-
-
-esp_err_t buzzer_init(void)
-{
-    // Configuration structure for GPIO
-    gpio_config_t io_conf = {
-        .intr_type = GPIO_INTR_DISABLE, // Disable interrupt
-        .mode = GPIO_MODE_OUTPUT,       // Set as output mode
-        .pin_bit_mask = (1ULL << BUZZER_GPIO), // Bit mask for GPIO2
-        .pull_down_en = GPIO_PULLDOWN_DISABLE, // Disable pull-down
-        .pull_up_en = GPIO_PULLUP_DISABLE,     // Disable pull-up
-    };
-
-    // Configure GPIO
-    esp_err_t err = gpio_config(&io_conf);
-    if (err != ESP_OK) {
-        // Handle error
-        return err;
-    }
-
-    return ESP_OK; // Initialization successful
-}
-
 //------------------------------ PUBLIC FUNCTIONS -----------------------------
 void morse_init(void)
 {
@@ -126,15 +102,19 @@ void morse_init(void)
 }
 
 //---------------------------- PRIVATE FUNCTIONS ------------------------------
-static void send_sos_ticks (){
-
-}
-
 static void _morse_task(void *p_parameter) {
 
     int morse_event;
     
     for (;;) {
-        gpio_set_level (BUZZER_GPIO, 0);
+        if((xQueueReceive(p_morse_queue, &morse_event, portMAX_DELAY) == pdTRUE)){
+            printf("Morse event received %d\n", morse_event);
+        }else {
+            printf("Morse failed\n");
+        }
+        // int i = 0;
+        // for(int j = 0; true; j++){
+        //     ESP_LOGI(TAG, "%d", j);
+        // }
     }
 }
